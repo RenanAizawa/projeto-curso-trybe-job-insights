8a2861867dc6ee165debf418b763d11894773834
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2023-01-23 14:54:20 -0400

    [feat]: adicionado função para leitura de arquivo csv

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,5 +1,6 @@
 from functools import lru_cache
 from typing import List, Dict
+import csv
 
 
 @lru_cache
@@ -16,6 +17,9 @@ def read(path: str) -> List[Dict]:
     list
         List of rows as dicts
     """
+    with open(path) as file:
+        list = csv.DictReader(file)
+        return list
     raise NotImplementedError
 
 

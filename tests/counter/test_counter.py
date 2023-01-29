9c6e5558724ae2c090b447e34ef0029842ed4ec0
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2023-01-29 16:53:23 -0400

    [fix]: correção na função test_counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -2,8 +2,13 @@ from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    data = count_ocurrences("data/jobs.csv", "salary")
-    print("data >>>>>>>>", data)
-    assert type(data) == int
-    assert data >= 0
-    assert data == 324
+    data1 = count_ocurrences("data/jobs.csv", "JavaScript")
+    data2 = count_ocurrences("data/jobs.csv", "Python")
+    print("data >>>>>>>>", data1)
+    print("data 2 >>>>>>>", data2)
+    assert type(data1) == int
+    assert type(data2) == int
+    assert data1 >= 0
+    assert data2 >= 0
+    assert data1 == 122
+    assert data2 == 1639

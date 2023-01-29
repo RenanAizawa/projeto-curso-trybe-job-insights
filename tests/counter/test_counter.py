58fa5f922c9d02de3bcd9cc37989a439c9bda3b9
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2023-01-29 16:45:39 -0400

    [test]: implementado funcionalidade do test_counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,9 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    data = count_ocurrences("data/jobs.csv", "salary")
+    print("data >>>>>>>>", data)
+    assert type(data) == int
+    assert data >= 0
+    assert data == 324
